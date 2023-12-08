af64871287e0ee52645300938d2687f5db556feb
tymtsias <premo1313@gmail.com>
2023-12-08 19:01:29 +0200

    inor change

--- cypress/models/zeroLoansModels.js
+++ cypress/models/zeroLoansModels.js
@@ -11,7 +11,7 @@ export const visitPage = () => {
 export const testScenario = () => {
   selectors.loginButton().should("be.visible").click();
   selectors.metamaskButton().should("be.visible").click();
-  cy.acceptMetamaskAccess().should("be.true");
+  // cy.acceptMetamaskAccess().should("be.true");
   selectors.walletButton().should("be.visible").click();
   selectors.walletConnectionBadge().should("be.visible").and("have.text", "Connected");
 };
